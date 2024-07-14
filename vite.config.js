510898e95110f23710839884fd1bc057a2fda872
amirhoseinshagholani <sh.amirhosein@gmail.com>
2024-07-14 16:22:41 +0330

    made login ui

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve:{
+    alias:{
+      '@public':path.resolve(__dirname,'./public'),
+      '@assets':path.resolve(__dirname,'./src/assets')
+    }
+  }
+})
